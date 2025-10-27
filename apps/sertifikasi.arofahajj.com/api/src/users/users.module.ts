b5c8f1573fb77cf3f1c48544a29608c593c40739
gunara10 <157656874+gunara10@users.noreply.github.com>
2025-10-28 02:48:29 +0700

    Import DatabaseModule in UsersModule
    Added DatabaseModule import to UsersModule for dependency injection.

--- apps/sertifikasi.arofahajj.com/api/src/users/users.module.ts
+++ apps/sertifikasi.arofahajj.com/api/src/users/users.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { DatabaseModule } from '../config/database.module'; // optional, DatabaseModule sudah @Global
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [UsersController],
   providers: [UsersService],
-  exports: [UsersService],
+  exports: [UsersService], // <-- penting untuk AuthModule
 })
 export class UsersModule {}
